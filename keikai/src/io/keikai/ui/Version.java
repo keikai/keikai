864d853b6ec11e3a9e830c6182a9cfbb763be252
hawkchen <hawkchen@potix.com>
2020-01-07 17:46:47 +0800

    advance to 5.0.0 for official release

--- keikai/src/io/keikai/ui/Version.java
+++ keikai/src/io/keikai/ui/Version.java
@@ -29,7 +29,7 @@ import org.zkoss.lang.Classes;
 public class Version {
 	/** Returns the version UID.
 	 */
-	public static final String UID = "5.0.0-Beta2";
+	public static final String UID = "5.0.0";
 	
 	/** Features. */
 	private static final boolean
